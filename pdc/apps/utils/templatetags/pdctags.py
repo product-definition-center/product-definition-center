544f52742349703cc311201dfc6649e49b9178df
Lubomír Sedlář <lsedlar@redhat.com>
2015-10-22 09:37:53 +0200

    Reduce duplication with custom template tag
    The bootstrap modal dialog markup is quite verbose and duplicate in many
    places. It is replaced with template tag that loads it from a separate
    file and fills in the contents. The button that opens the dialog is
    moved into a separate template.

--- pdc/apps/utils/templatetags/pdctags.py
+++ pdc/apps/utils/templatetags/pdctags.py
@@ -5,6 +5,7 @@
 #
 from django import template
 from django.conf import settings
+from django.template.loader import get_template
 
 from pdc import get_version
 
@@ -24,7 +25,39 @@ def login_url(redirect=None):
         url += '?next=' + redirect
     return url
 
+
+def do_help_modal(parser, token):
+    nodelist = parser.parse(('endhelpmodal',))
+    parser.delete_first_token()
+    return HelpModalNode(nodelist)
+
+
+class HelpModalNode(template.Node):
+    """Create a Bootstrap modal dialog with help.
+
+    Use the ``help_button.html`` template to create a button to display this
+    dialog.
+
+    To use it, just wrap the dialog contents with template tags like ::
+
+        {% helpmodal %}
+            Contents of the dialog...
+        {% endhelpmodal %}
+
+    On each page, there can be only one dialog created using this tag.
+    """
+    def __init__(self, nodelist):
+        self.nodelist = nodelist
+
+    def render(self, context):
+        t = get_template('help_modal.html')
+        output = self.nodelist.render(context)
+        context['content'] = output
+        return t.render(context)
+
+
 register = template.Library()
 
 register.simple_tag(pdc_version)
 register.simple_tag(login_url)
+register.tag('helpmodal', do_help_modal)
