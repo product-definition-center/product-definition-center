05280efdee0c347d59c4e8272d955388fb8b1a76
Lubomír Sedlář <lsedlar@redhat.com>
2015-11-04 12:15:08 +0100

    Use popovers instead of dialogs for help
    The popovers are triggered by hovering over the icon. Once the cursor
    leaves the icon, the popover is hidden. The only exception is if the
    cursor moved over the actual popover, in which case it will disappear
    only after the cursor left it.
    
    JIRA: PDC-1115

--- pdc/apps/utils/templatetags/pdctags.py
+++ pdc/apps/utils/templatetags/pdctags.py
@@ -26,31 +26,31 @@ def login_url(redirect=None):
     return url
 
 
-def do_help_modal(parser, token):
-    nodelist = parser.parse(('endhelpmodal',))
+def do_help_popover(parser, token):
+    nodelist = parser.parse(('endhelppopover',))
     parser.delete_first_token()
-    return HelpModalNode(nodelist)
+    return HelpPopoverNode(nodelist)
 
 
-class HelpModalNode(template.Node):
-    """Create a Bootstrap modal dialog with help.
+class HelpPopoverNode(template.Node):
+    """Create a Bootstrap popover with help.
 
     Use the ``help_button.html`` template to create a button to display this
     dialog.
 
     To use it, just wrap the dialog contents with template tags like ::
 
-        {% helpmodal %}
-            Contents of the dialog...
-        {% endhelpmodal %}
+        {% helppopover %}
+            Contents of the popover...
+        {% endhelppopover %}
 
-    On each page, there can be only one dialog created using this tag.
+    On each page, there can be only one popover created using this tag.
     """
     def __init__(self, nodelist):
         self.nodelist = nodelist
 
     def render(self, context):
-        t = get_template('help_modal.html')
+        t = get_template('help_popover.html')
         output = self.nodelist.render(context)
         context['content'] = output
         return t.render(context)
@@ -60,4 +60,4 @@ register = template.Library()
 
 register.simple_tag(pdc_version)
 register.simple_tag(login_url)
-register.tag('helpmodal', do_help_modal)
+register.tag('helppopover', do_help_popover)
