8f39826f93f3e0e8674a0c5232df6740618b7425
Xiangyang Chu <xchu@redhat.com>
2015-11-11 13:07:03 +0800

    Add Release Component Contact client plugin
    Including:
      - list
      - info
      - create
      - delete
      - delete-match
    
    Tests added.

--- pdc_client/plugins/contact.py
+++ pdc_client/plugins/contact.py
@@ -13,6 +13,25 @@ from pdc_client.plugin_helpers import (PDCClientPlugin,
                                        add_create_update_args)
 
 
+def print_component_contacts(component_contacts):
+    fmt = '{:<10} {:40} {:10} {:25} {}'
+    print fmt.format('ID', 'Component', 'Role', 'Email', 'Name')
+    for component_contact in component_contacts:
+        print fmt.format(
+            component_contact['id'],
+            'release' in component_contact['component'] and
+            ''.join([component_contact['component']['release'],
+                     '/',
+                     component_contact['component']['name']]) or
+            component_contact['component'],
+            component_contact['role'],
+            component_contact['contact']['email'],
+            'username' in component_contact['contact'] and
+            component_contact['contact']['username'] or
+            component_contact['contact']['mail_name']
+        )
+
+
 class GlobalComponentContactPlugin(PDCClientPlugin):
     def register(self):
         self.set_command('global-component-contact')
@@ -63,21 +82,7 @@ class GlobalComponentContactPlugin(PDCClientPlugin):
             print json.dumps(list(global_component_contacts))
             return
         if global_component_contacts:
-            self.print_component_contacts(global_component_contacts)
-
-    def print_component_contacts(self, global_component_contacts):
-        fmt = '{:<10} {:25} {:10} {:25} {}'
-        print fmt.format('ID', 'Component', 'Role', 'Email', 'Name')
-        for global_component_contact in global_component_contacts:
-            print fmt.format(
-                global_component_contact['id'],
-                global_component_contact['component'],
-                global_component_contact['role'],
-                global_component_contact['contact']['email'],
-                'username' in global_component_contact['contact'] and
-                global_component_contact['contact']['username'] or
-                global_component_contact['contact']['mail_name']
-            )
+            print_component_contacts(global_component_contacts)
 
     def info_global_component_contact(self, args, global_component_contact_id=None):
         global_component_contact_id = global_component_contact_id or args.global_component_contact_id
@@ -126,7 +131,112 @@ class GlobalComponentContactPlugin(PDCClientPlugin):
             print "No match, nothing to do."
         else:
             print "Multi matches, please delete via ID or provide more restrictions."
-            self.print_component_contacts(global_component_contacts)
+            print_component_contacts(global_component_contacts)
 
 
-PLUGIN_CLASSES = [GlobalComponentContactPlugin]
+class ReleaseComponentContactPlugin(PDCClientPlugin):
+    def register(self):
+        self.set_command('release-component-contact')
+
+        list_parser = self.add_action('list', help='list all release component contacts')
+        self.add_query_arguments(list_parser)
+        list_parser.set_defaults(func=self.list_release_component_contacts)
+
+        info_parser = self.add_action('info', help='display details of a release component contact')
+        info_parser.add_argument('release_component_contact_id', metavar='RELEASE_COMPONENT_CONTACT_ID')
+        info_parser.set_defaults(func=self.info_release_component_contact)
+
+        create_parser = self.add_action('create', help='create new release component contact')
+        self.add_create_arguments(create_parser, required=True)
+        create_parser.set_defaults(func=self.create_release_component_contact)
+
+        delete_parser = self.add_action('delete', help='delete a release component contact')
+        delete_parser.add_argument('release_component_contact_id', metavar='RELEASE_COMPONENT_CONTACT_ID')
+        delete_parser.set_defaults(func=self.delete_release_component_contact)
+
+        query_delete_parser = self.add_action('delete-match', help='delete unique matched release component contact')
+        self.add_query_arguments(query_delete_parser)
+        query_delete_parser.set_defaults(func=self.delete_matched_release_component_contact)
+
+    def add_query_arguments(self, parser, required=False):
+        filters = ('release dist_git_branch global_component component contact role email'.split())
+        for arg in filters:
+            parser.add_argument('--' + arg.replace('_', '-'), dest='filter_' + arg)
+
+    def add_create_arguments(self, parser, required=True):
+        add_create_update_args(parser,
+                               {'component__release': {'arg': 'release'},
+                                'component__name': {'arg': 'component'},
+                                'role': {},
+                                'contact__email': {'arg': 'email'}},
+                               {'contact__username': {'arg': 'username'},
+                                'contact__mail_name': {'arg': 'mail-name'},
+                                },
+                               required)
+
+    def list_release_component_contacts(self, args):
+        filters = extract_arguments(args, prefix='filter_')
+        if not filters:
+            sys.stderr.write('At least some filter must be used.\n')
+            sys.exit(1)
+        release_component_contacts = get_paged(self.client['release-component-contacts']._, **filters)
+
+        if args.json:
+            print json.dumps(list(release_component_contacts))
+            return
+        if release_component_contacts:
+            print_component_contacts(release_component_contacts)
+
+    def info_release_component_contact(self, args, release_component_contact_id=None):
+        release_component_contact_id = release_component_contact_id or args.release_component_contact_id
+        release_component_contact = self.client['release-component-contacts'][release_component_contact_id]._()
+
+        if args.json:
+            print json.dumps(release_component_contact)
+            return
+
+        fmt = '{:20} {}'
+        print fmt.format('ID:', release_component_contact['id'])
+        print fmt.format('Component:', ''.join([release_component_contact['component']['release'],
+                                                '/',
+                                                release_component_contact['component']['name']]))
+        print fmt.format('Role:', release_component_contact['role'])
+        print 'Contact:'
+        for name in ('username', 'mail_name'):
+            if name in release_component_contact['contact']:
+                print ''.join(['\tName:\t', release_component_contact['contact'][name]])
+        print ''.join(['\tEmail:\t', release_component_contact['contact']['email']])
+
+    def create_release_component_contact(self, args):
+        data = extract_arguments(args)
+        self.logger.debug('Creating release component contact with data %r', data)
+        response = self.client['release-component-contacts']._(data)
+        self.info_release_component_contact(args, response['id'])
+
+    def delete_release_component_contact(self, args):
+        data = extract_arguments(args)
+        self.logger.debug('Deleting release component contact: %s',
+                          args.release_component_contact_id)
+        self.client['release-component-contacts'
+                    ][args.release_component_contact_id]._("DELETE", data)
+
+    def delete_matched_release_component_contact(self, args):
+        filters = extract_arguments(args, prefix='filter_')
+        if not filters:
+            sys.stderr.write('At least some filter must be used.\n')
+            sys.exit(1)
+        release_component_contacts = get_paged(self.client['release-component-contacts']._, **filters)
+
+        release_component_contacts = list(release_component_contacts)
+        match_count = len(release_component_contacts)
+        if match_count == 1:
+            self.client['release-component-contacts'
+                        ][release_component_contacts[0]['id']]._("DELETE", {})
+        elif match_count < 1:
+            print "No match, nothing to do."
+        else:
+            print "Multi matches, please delete via ID or provide more restrictions."
+            print_component_contacts(release_component_contacts)
+
+
+PLUGIN_CLASSES = [GlobalComponentContactPlugin, ReleaseComponentContactPlugin]
--- pdc_client/tests/contact/tests.py
+++ pdc_client/tests/contact/tests.py
@@ -103,3 +103,112 @@ class GlobalComponentContactTestCase(CLITestCase):
         with self.expect_output('global_component_contact/list.json', parse_json=True):
             self.runner.run(['--json', 'global-component-contact', 'list',
                              '--role', 'pm'])
+
+
+class ReleaseComponentContactTestCase(CLITestCase):
+    def setUp(self):
+        self.runner = Runner()
+        self.runner.setup()
+        self.detail = {
+            'id': '1',
+            'component': {
+                'name': 'Test Release Component',
+                'release': 'test-release'
+            },
+            'role': 'pm',
+            'contact': {
+                'username': 'Test Contact',
+                'email': 'test_contact@example.com',
+            }
+        }
+
+    def _setup_detail(self, api):
+        api.add_endpoint('release-component-contacts/1', 'GET', self.detail)
+
+    def test_list_without_filters(self, api):
+        with self.expect_failure():
+            self.runner.run(['release-component-contact', 'list'])
+
+    def test_list_multi_page(self, api):
+        api.add_endpoint('release-component-contacts', 'GET', [
+            {'id': x,
+             'component': {
+                 'name': 'Test Release Component %s' % x,
+                 'release': 'test-release'
+             },
+             'role': 'watcher',
+             'contact': {
+                 'username': 'Test Contact',
+                 'email': 'test_contact@example.com',
+             }}
+            for x in range(1, 26)
+        ])
+        with self.expect_output('release_component_contact/list_multi_page.txt'):
+            self.runner.run(['release-component-contact', 'list',
+                             '--role', 'watcher'])
+        self.assertEqual(api.calls['release-component-contacts'],
+                         [('GET', {'page': 1, 'role': 'watcher'}),
+                          ('GET', {'page': 2, 'role': 'watcher'})])
+
+    def test_detail(self, api):
+        self._setup_detail(api)
+        with self.expect_output('release_component_contact/detail.txt'):
+            self.runner.run(['release-component-contact', 'info', '1'])
+        self.assertDictEqual(api.calls,
+                             {'release-component-contacts/1': [('GET', {})]})
+
+    def test_delete(self, api):
+        api.add_endpoint('release-component-contacts/1', 'DELETE', None)
+        with self.expect_output('release_component_contact/empty.txt'):
+            self.runner.run(['release-component-contact', 'delete', '1'])
+        self.assertDictEqual(api.calls,
+                             {'release-component-contacts/1': [('DELETE', {})]})
+
+    def test_delete_matched(self, api):
+        api.add_endpoint('release-component-contacts', 'GET', [self.detail])
+        api.add_endpoint('release-component-contacts/1', 'DELETE', None)
+        with self.expect_output('release_component_contact/empty.txt'):
+            self.runner.run(['release-component-contact', 'delete-match', '--role', 'pm'])
+        self.assertDictEqual(api.calls,
+                             {'release-component-contacts': [('GET', {'page': 1,
+                                                                      'role': 'pm'})],
+                              'release-component-contacts/1': [('DELETE', {})]})
+
+    def test_create(self, api):
+        api.add_endpoint('release-component-contacts', 'POST', self.detail)
+        self._setup_detail(api)
+        with self.expect_output('release_component_contact/detail.txt'):
+            self.runner.run(['release-component-contact', 'create',
+                             '--release', 'test-release',
+                             '--component', 'Test Release Component',
+                             '--role', 'pm',
+                             '--username', 'Test Contact',
+                             '--email', 'test_contact@example.com'])
+        self.assertDictEqual(api.calls,
+                             {'release-component-contacts':
+                                 [('POST',
+                                   {
+                                       'component': {
+                                           'name': 'Test Release Component',
+                                           'release': 'test-release'
+                                       },
+                                       'role': 'pm',
+                                       'contact': {
+                                           'username': 'Test Contact',
+                                           'email': 'test_contact@example.com'
+                                       }
+                                   })],
+                              'release-component-contacts/1': [('GET', {})]})
+
+    def test_info_json(self, api):
+        self._setup_detail(api)
+        with self.expect_output('release_component_contact/detail.json', parse_json=True):
+            self.runner.run(['--json', 'release-component-contact', 'info', '1'])
+        self.assertDictEqual(api.calls,
+                             {'release-component-contacts/1': [('GET', {})]})
+
+    def test_list_json(self, api):
+        api.add_endpoint('release-component-contacts', 'GET', [self.detail])
+        with self.expect_output('release_component_contact/list.json', parse_json=True):
+            self.runner.run(['--json', 'release-component-contact', 'list',
+                             '--role', 'pm'])
