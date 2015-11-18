b1e77324e78db4484f464d9ef7cfd87a3d4cf926
Lubomír Sedlář <lsedlar@redhat.com>
2015-11-18 10:22:22 +0100

    Remove client documentation
    The document should move to pdc-client repo. The autodoc extension that
    was used in the document lead to errors when trying to import modules
    that are not present in the repo now. Since there was no other use of
    the extension, autodoc is disabled now.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ from pdc import get_version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
