371857564aeab64407cbd3a4a54009d514a97c51
Nils Philippsen <nils@redhat.com>
2016-07-12 11:50:39 +0200

    API end points are plural, not singular
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- pdc/apps/tree/routers.py
+++ pdc/apps/tree/routers.py
@@ -2,5 +2,5 @@ from . import views
 from pdc.apps.utils.SortedRouter import router
 
 
-router.register(r'tree', views.TreeViewSet)
-router.register(r'unreleasedvariant', views.UnreleasedVariantViewSet)
+router.register(r'trees', views.TreeViewSet)
+router.register(r'unreleasedvariants', views.UnreleasedVariantViewSet)
