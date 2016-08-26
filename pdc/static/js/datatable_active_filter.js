1529f054a769ad659d3f2007ec95e7c4ad7c8fe0
bliu <bliu@redhat.com>
2016-08-26 16:01:41 +0800

    Display all/active/inactive
    For releases, product versions and products APIs, add selector attribute
    in datatable, which instead of the Column Name. And click the seletor
    to filter and display all/active/inactive status.
    
    1, Remove the method of pagination once we used, and apply the Datatables'
    method(reference to jQuery), which complete pagination automatically.
    2, Set /all/ as the default value.
    
    JIRA: PDC-1589

---
+++ pdc/static/js/datatable_active_filter.js
@@ -0,0 +1,22 @@
+(function () {
+    "use strict";
+    $(document).ready(function() {
+        if ($('select').is('#active_filter')){
+            var page_size = parseInt($('select').attr("value"));
+            var table = $('table').dataTable(
+                {   "pageLength" : page_size,
+                    "ordering": false
+                });
+            $('.dataTables_filter').hide();
+            $('#active_filter').prop('selectedIndex',0);
+            $('#active_filter').on('change', function() {
+                if (this.value !== "All") {
+                    table.fnFilter( $(this).val());
+                }
+                else {
+                    table.fnFilter("");
+                }
+            });
+        }
+    });
+})();
