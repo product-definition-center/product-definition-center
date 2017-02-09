5ce7f70986e47ec4ce285d60411837ad0660dca4
Ralph Bean <rbean@redhat.com>
2017-02-09 08:41:35 -0500

    Fix app loading in django 1.9 like in #419.

--- pdc/apps/tree/apps.py
+++ pdc/apps/tree/apps.py
@@ -4,11 +4,11 @@
 # http://opensource.org/licenses/MIT
 #
 from django.apps import AppConfig
-from pdc.apps.utils.utils import connect_app_models_pre_save_signal
 
 
 class TreeConfig(AppConfig):
     name = 'pdc.apps.tree'
 
     def ready(self):
+        from pdc.apps.utils.utils import connect_app_models_pre_save_signal
         connect_app_models_pre_save_signal(self)
