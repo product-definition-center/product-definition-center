2d7a9747f4db050a536017046a650fefb97eb028
Lukas Holecek <lholecek@redhat.com>
2018-01-22 14:37:00 +0100

    Add django_filters to INSTALLED_APPS
    Using filter backend "django_filters.rest_framework.DjangoFilterBackend"
    requires correct template path to be found.
    
    Fixes exception:
    
        TemplateDoesNotExist: django_filters/rest_framework/form.html

--- pdc/settings_common.py
+++ pdc/settings_common.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
