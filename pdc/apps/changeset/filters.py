3dec8fa30810786a6d04cb87e3eb0a2218d98e3b
Lukas Holecek <lholecek@redhat.com>
2018-02-05 13:12:29 +0100

    Fix changeset date/time filter warnings
    Fixes following Django warning.
    
        RuntimeWarning: DateTimeField Changeset.committed_on received
            a naive datetime ... while time zone support is active.

--- pdc/apps/changeset/filters.py
+++ pdc/apps/changeset/filters.py
@@ -5,10 +5,26 @@
 #
 import django.forms.widgets as widgets
 
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
+
 import django_filters
 
 from pdc.apps.common.filters import MultiValueFilter, value_is_not_empty
 from . import models
+
+
+def _date_time_with_timezone(value):
+    date_time = parse_datetime(value)
+    if date_time is None:
+        return value
+    return timezone.make_aware(date_time)
+
+
+def _filter_committed(qs, lookup_expr, value):
+    date_time = _date_time_with_timezone(value)
+    filter_args = {'committed_on__' + lookup_expr: date_time}
+    return qs.filter(**filter_args).distinct()
 
 
 class ChangesetFilterSet(django_filters.FilterSet):
@@ -22,11 +38,11 @@ class ChangesetFilterSet(django_filters.FilterSet):
 
     @value_is_not_empty
     def filter_committed_since(self, qs, name, value):
-        return qs.filter(committed_on__gte=value).distinct()
+        return _filter_committed(qs, 'gte', value)
 
     @value_is_not_empty
     def filter_committed_until(self, qs, name, value):
-        return qs.filter(committed_on__lte=value).distinct()
+        return _filter_committed(qs, 'lte', value)
 
     class Meta:
         model = models.Changeset
